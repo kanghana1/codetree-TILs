6715e3651caf32a656fd1a253559ac9eb0dd34b4
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-06-13 11:57:53 +0900

    Title: 화면에 출력, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

--- 240613/화면에 출력/output-to-screen.java
+++ 240613/화면에 출력/output-to-screen.java
@@ -1,22 +1,20 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
 
     static int goal;
     static Queue<int[]> queue = new LinkedList<>();
     static boolean[][] isVisited;
 
-
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int goal = Integer.parseInt(br.readLine());
+        goal = Integer.parseInt(br.readLine());
         System.out.println(bfs());
-
     }
 
     public static int bfs() {
-        queue.add(new int[] {1, 0, 1}); // 화면문자수, 클립보드문자수, 연산수
+        queue.add(new int[] {1, 0, 0}); // 화면문자수, 클립보드문자수, 연산수
         isVisited = new boolean[1001][1001];
         isVisited[1][0] = true; // [화면문자수][클립보드 문자수]로 채워넣기
 
@@ -28,25 +26,24 @@ public class Main {
 
             if (screen == goal) return count;
 
-            // copy
-            if (clipboard != screen && !isVisited[screen][screen]) { // 복사하면 화면문자수 == 클립보드 문자수
+            // 복사 연산
+            if (screen > 0 && !isVisited[screen][screen]) { // 복사하면 화면문자수 == 클립보드 문자수
                 isVisited[screen][screen] = true;
                 queue.add(new int[] {screen, screen, count + 1});
             }
 
-            // paste
-            if (!isVisited[screen + clipboard][clipboard] && clipboard > 0 && screen + clipboard <= 1000) {
+            // 붙여넣기 연산
+            if (clipboard > 0 && screen + clipboard <= 1000 && !isVisited[screen + clipboard][clipboard]) {
                 isVisited[screen + clipboard][clipboard] = true;
                 queue.add(new int[] {screen + clipboard, clipboard, count + 1});
             }
 
-            // delete
-            if (!isVisited[screen - 1][clipboard] && screen > 0) {
+            // 삭제 연산
+            if (screen > 1 && !isVisited[screen - 1][clipboard]) {
                 isVisited[screen - 1][clipboard] = true;
                 queue.add(new int[] {screen - 1, clipboard, count + 1});
             }
         }
-        return 0;
-
+        return -1;
     }
 }
