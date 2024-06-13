086a90adb4b9f5904c93a0f3dc8487db69ef6f7e
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-06-13 11:20:11 +0900

    Title: 화면에 출력, Time: 95ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240613/화면에 출력/output-to-screen.java
+++ 240613/화면에 출력/output-to-screen.java
@@ -3,50 +3,50 @@ import java.io.*;
 public class Main {
 
     static int goal;
-    static int copyNum;
     static Queue<int[]> queue = new LinkedList<>();
+    static boolean[][] isVisited;
 
 
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int goal = Integer.parseInt(br.readLine());
-        System.out.println(bfs(1));
+        System.out.println(bfs());
 
     }
 
-    public static int bfs(int init) {
-        queue.add(new int[] {init, 1}); // 값, cnt
+    public static int bfs() {
+        queue.add(new int[] {1, 0, 1}); // 화면문자수, 클립보드문자수, 연산수
+        isVisited = new boolean[1001][1001];
+        isVisited[1][0] = true; // [화면문자수][클립보드 문자수]로 채워넣기
+
         while(!queue.isEmpty()) {
             int[] now = queue.poll();
-            if (now[0] == goal) return now[1];
+            int screen = now[0];
+            int clipboard = now[1];
+            int count = now[2];
 
-            for (int i = 1 ; i <= 3 ; i++) {
-                queue.add(copy(now[0], now[1]));
-                queue.add(paste(now[0], now[1]));
-                queue.add(delete(now[0], now[1]));
+            if (screen == goal) return count;
+
+            // copy
+            if (!isVisited[screen][screen]) { // 복사하면 화면문자수 == 클립보드 문자수
+                isVisited[screen][screen] = true;
+                queue.add(new int[] {screen, screen, count + 1});
             }
 
+            // paste
+            if (!isVisited[screen + clipboard][clipboard] && clipboard > 0 && screen + clipboard <= 1000) {
+                isVisited[screen + clipboard][clipboard] = true;
+                queue.add(new int[] {screen + clipboard, clipboard, count + 1});
+            }
+
+            // delete
+            if (!isVisited[screen - 1][clipboard] && screen > 0) {
+                isVisited[screen - 1][clipboard] = true;
+                queue.add(new int[] {screen - 1, clipboard, count + 1});
+            }
         }
         return 0;
 
     }
-
-    public static int[] copy(int n, int cnt) { // 값, cnt
-        copyNum = n;
-        return new int[] {n, cnt + 1};
-    }
-    public static int[] paste(int now, int cnt) {
-        int[] arr = new int[2];
-        arr[0] = now + copyNum;
-        arr[1] = cnt + 1;
-        return arr;
-    }
-    public static int[] delete(int now, int cnt) {
-        int[] arr = new int[2];
-        arr[0] = now - 1;
-        arr[1] = cnt + 1;
-        return arr;
-    }
-
 }
