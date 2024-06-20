5365d016a2c99972f9f08914710f713ccaae42d4
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-06-20 09:32:23 +0900

    Title: n x m 표 이동 7, Time: 109ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240620/n x m 표 이동 7/move-n-x-m-table-7.java
+++ 240620/n x m 표 이동 7/move-n-x-m-table-7.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
 
     static int rNum;
@@ -10,10 +11,9 @@ public class Main {
     static int[] initLeftTop;
     static int[] goal;
 
-    static int[] dx = {0, 1, 0 , -1}; // 북 동 남 서
+    static int[] dx = {0, 1, 0, -1}; // 북 동 남 서
     static int[] dy = {-1, 0, 1, 0};
 
-    static int count = 0;
     static int h;
     static int w;
 
@@ -25,76 +25,69 @@ public class Main {
         maze = new int[rNum][cNum];
         isVisit = new boolean[rNum][cNum];
 
-        for (int i = 0 ; i < rNum ; i++) {
+        for (int i = 0; i < rNum; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0 ; j < cNum ; j++) {
-                int input = Integer.parseInt(st.nextToken());
-                maze[i][j] = input;
-                if (input == 1) {
-                    isVisit[i][j] = true;
-                }
+            for (int j = 0; j < cNum; j++) {
+                maze[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
+
         st = new StringTokenizer(br.readLine());
         h = Integer.parseInt(st.nextToken());
         w = Integer.parseInt(st.nextToken());
         initLeftTop = new int[] {Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1};
         goal = new int[] {Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1};
 
-        bfs();
-        System.out.print(count);
+        int result = bfs();
+        System.out.print(result);
     }
 
-    public static void bfs() {
-        // 기준은 initLeftTop
+    public static int bfs() {
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(initLeftTop);
+        queue.add(new int[]{initLeftTop[0], initLeftTop[1], 0}); // {y, x, count}
         isVisit[initLeftTop[0]][initLeftTop[1]] = true;
 
-        int y = 0;
-        int x = 0;
-
-        while (!queue.isEmpty() || (y == goal[0] && x == goal[1])) {
+        while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            int rightBotY = current[0] + h - 1;
-            int rightBotX = current[1] + w - 1;
+            int cy = current[0];
+            int cx = current[1];
+            int count = current[2];
 
-            for (int i = 0 ; i < 4 ; i++) { // 북 동 남 서
-                y = current[0] + dy[i];
-                x = current[1] + dx[i];
+            if (cy == goal[0] && cx == goal[1]) {
+                return count;
+            }
 
-                boolean isWall = false;
-                if (canGo(y, x)) {
-                    if (i == 0 || i == 2) {
-                        for (int j = x ; j <= x + w - 1 ; j++) {
-                            if (maze[y][j] == 1) {
-                                isWall = true;
-                                break;
-                            }
-                        }
-                    }
-                    else if (i == 1 || i == 3) {
-                        for (int j = y ; j <= y + h - 1 ; j++) {
-                            if (maze[i][x] == 1) {
-                                isWall = true;
-                                break;
-                            }
-                        }
-                    }
+            for (int i = 0; i < 4; i++) { // 북 동 남 서
+                int ny = cy + dy[i];
+                int nx = cx + dx[i];
 
-                    if (!isWall) {
-                        queue.add(new int[] {y, x});
-                        isVisit[y][x] = true;
-                        count++;
-                    }
+                if (canMove(ny, nx)) {
+                    isVisit[ny][nx] = true;
+                    queue.add(new int[]{ny, nx, count + 1});
                 }
-
             }
         }
+
+        return -1; // 이동할 수 없는 경우
     }
 
-    public static boolean canGo(int yIdx, int xIdx) {
-        return yIdx >= 0 && yIdx < rNum && xIdx >= 0 && xIdx < cNum && !isVisit[yIdx][xIdx];
+    public static boolean canMove(int y, int x) {
+        if (y < 0 || y + h > rNum || x < 0 || x + w > cNum) {
+            return false;
+        }
+        if (isVisit[y][x]) {
+            return false;
+        }
+
+        // 직사각형 영역 검사
+        for (int i = y; i < y + h; i++) {
+            for (int j = x; j < x + w; j++) {
+                if (maze[i][j] == 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
