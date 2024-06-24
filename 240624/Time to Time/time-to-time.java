43dccecf014ce8e7f0ea45f8492b1b211c8c2991
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-06-24 22:45:04 +0900

    Title: Time to Time, Time: 119ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240624/Time to Time/time-to-time.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        
+        if (d > b) {
+            d += 60;
+            c--;
+        }
+        System.out.println((c - a) * 60 + (d - b));
+    }
+}
