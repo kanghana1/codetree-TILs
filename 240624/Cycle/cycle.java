456727e260dc0bd466842e1551318df6fa9b6dda
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-06-24 23:46:16 +0900

    Title: Cycle, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240624/Cycle/cycle.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        List<Integer> lst = new ArrayList<>();
+        lst.add(N);
+        int result = N;
+        int cnt = 0;
+        while (true) {
+            result = (result * N) % P;
+            if (lst.contains(result)) break;
+            lst.add(result);
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
