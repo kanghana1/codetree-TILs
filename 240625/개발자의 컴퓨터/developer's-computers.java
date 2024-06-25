e14982739191c465f7e2f7d3dd999a993671af7d
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-06-25 11:28:29 +0900

    Title: 개발자의 컴퓨터, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240625/개발자의 컴퓨터/developer's-computers.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static List<PC> lst = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0 ; i < N ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            lst.add(new PC(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+
+        int cnt = lst.get(0).numOfPc;
+        boolean overlap = true;
+        for (int i = 1 ; i < N ; i++) {
+            overlap = true;
+            for (int j = 0 ; j < i ; j++) {
+                if (lst.get(i).start > lst.get(j).end || lst.get(i).end < lst.get(j).start) { // 안 겹침
+                    overlap = false;
+                    if (lst.get(j).numOfPc < lst.get(i).numOfPc) {
+                        cnt = cnt - lst.get(j).numOfPc + lst.get(i).numOfPc;
+                    }
+                    break;
+                }
+            }
+            if (overlap) {
+                cnt += lst.get(i).numOfPc;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    static class PC {
+        private int start;
+        private int end;
+        private int numOfPc;
+
+        public PC(int start, int end, int numOfPc) {
+            this.start = start;
+            this.end = end;
+            this.numOfPc = numOfPc;
+        }
+    }
+}
