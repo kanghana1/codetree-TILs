02237072f980e6f38500c48c642af81200bb48b2
강하나 <107329874+kanghana1@users.noreply.github.com>
2024-07-10 10:40:41 +0900

    Title: 가장 긴 증가하는 부분 수열 3, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240710/가장 긴 증가하는 부분 수열 3/increasing-subsequence-3.java
+++ 240710/가장 긴 증가하는 부분 수열 3/increasing-subsequence-3.java
@@ -1,21 +1,26 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] input = new int[n];
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0 ; i < n ; i++) {
             input[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
         }
 
+        int result = 1;
         for (int i = 1 ; i < n ; i++) {
-            if (input[i] > input[i - 1]) dp[i] = dp[i - 1] + 1;
-            else dp[i] = dp[i - 1];
+            for (int j = 0 ; j < i ; j++) {
+                if (input[i] > input[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            result = Math.max(dp[i], result);
         }
-        System.out.println(dp[n - 1] + 1);
+        System.out.println(dp[n - 1]);
     }
 }
